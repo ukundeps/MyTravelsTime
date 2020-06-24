e0bce0621a53b5a2762048b7d19323185cb238b7
Ritu Singh <rosesinghritu@gmail.com>
2020-06-24 20:46:10 +0530

    Excel reading method added

--- src/main/java/com/mytravels/constant/Constant.java
+++ src/main/java/com/mytravels/constant/Constant.java
@@ -11,10 +11,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-
 public class Constant {
 	
 	public static WebDriver driver;
@@ -25,9 +21,9 @@ public class Constant {
 	public static Actions action;
 	public static Select select;
 	public static Properties prop ;
-	public static  ExtentHtmlReporter htmlReporter;
-	public static ExtentReports extent;
-	public static ExtentTest extentLog;
+	//public static  ExtentHtmlReporter htmlReporter;
+	//public static ExtentReports extent;
+	//public static ExtentTest extentLog;
 	
 
 }
